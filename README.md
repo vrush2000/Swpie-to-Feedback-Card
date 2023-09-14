c53645b82c6c89bbc98bee679cce2f9bbdb0e168
vrush2000 <syamsul8895@gmail.com>
2023-09-14 22:30:50 +0700

    change from wechat miniprogram to alipay miniprogram

--- README.md
+++ README.md
@@ -1,67 +1,30 @@
 # Swpie-to-Feedback-Card
-一个自制且无外部依赖的、用于原生微信小程序的左右滑动卡片选择喜好的UI组件。
-类似 Tinder、探探 和 Aloha 等软件的社交推荐选择交互操作。
-本组件基于原生微信元素和JS实现，不依赖于第三方库。
+A self-made UI component for the Alipay applet to slide cards left and right to select preferences.
+Social recommendation options like Tinder, Tantan, and Aloha interact with each other.
 
-## 基本使用示例
-复制 /plugins/components 文件夹中的 swipe-to-feedback 组件内容到你的微信小程序项目中的 components 文件夹内，
-并在app.json或要使用的页面的json配置文件中注册组件，并在需要使用组件的 wxml 文件中写入：
-
-```
-<swipe-to-feedback-card list="{{cardList}}"  empty-description="这里放入没有卡片时的描述" bindonlike="onLike" bindondislike="onDislike"/>
-
-```
-
-其中 cardList 是表示卡片内容的数组，格式样例如下:
 
 ```
 {
-	id: 0, // id 必须唯一不可重复
-	type: "image",
-	url: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=328517395,2303970886&fm=27&gp=0.jpg", // 卡片图片的 URL
-	title: "卡片1标题",
-	subtitle: "卡片1副标题",
-	description: "卡片1描述",
-	navigateUrl: "/pages/index/index" // 点击卡片时重定向导航的目标链接
-}, {
-	id: 1, // id 必须唯一不可重复
-	type: "image",
-	url: "http://img4.imgtn.bdimg.com/it/u=3300954562,1274487754&fm=26&gp=0.jpg", // 卡片图片的 URL
-	title: "卡片2标题",
-	subtitle: "卡片2副标题",
-	description: "卡片2描述",
-	navigateUrl: "/pages/index/index" // 点击卡片时重定向导航的目标链接
+	"id": i, // id must be unique and not repeatable
+	"type": "image",
+	"url": `https://picsum.photos/300/600?random=1`,
+	"title": `Card`,
+	"subtitle": `Card Subtitle`,
+	"description": `Card Description`,
+	"navigateUrl": `/pages/index/index` // Redirect navigation target link 
 }
 ```
 
-## 事件回调绑定
-示例中 xml 标签里的 bindonlike 和 bindondislike 是设置负责处理用户回调的函数的属性，
-其中 onLike 是用户触发喜欢操作的回调函数，onDislike 是用户触发不喜欢操作的回调函数。
-
-这两个回调函数均有一个输入，输入值为被操作的卡片对应的对象。
-例如：
-```
-{
-	id: 1, // id 必须唯一不可重复
-	type: "image",
-	url: "http://img4.imgtn.bdimg.com/it/u=3300954562,1274487754&fm=26&gp=0.jpg", // 卡片图片的 URL
-	title: "卡片2标题",
-	subtitle: "卡片2副标题",
-	description: "卡片2描述",
-	navigateUrl: "/pages/index/index" // 点击卡片时重定向导航的目标链接
-}
-```
 
 ## Demo
-组件使用效果示例：
-
-<img src="https://github.com/Cheelem/Swpie-to-Feedback-Card/blob/master/images/swipe-to-feed-back-card-sample.gif?raw=true"  height="647" width="366"> <img src="https://github.com/Cheelem/Swpie-to-Feedback-Card/blob/master/images/swipe-to-feed-back-card-sample2.gif?raw=true"  height="647" width="366">
-
-## 其他声明
-示例中使用了随机从网络搜寻的两个图片，若有存在版权问题，请即告知。
-
-## 开源协定
-本项目遵循 MIT 开源协议之规定。
+<video src="./assets//card-swipe.mp4" height="647" width="366"  autoplay loop muted>
+</video>
+<img src="./assets/card-left.jpeg" height=" 647" width="366">
+<img src="./assets/card-right.jpeg" height="647" width="366">
 
 
+## Other statements
+The example uses two images randomly searched from the Internet. If there are any copyright issues, please let us know immediately.
 
+## Open Source Agreement
+This project follows the provisions of the MIT Open Source License.
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,257 @@
-var plugin = requirePlugin("myPlugin")
 Page({
-  onLoad: function() {
-    plugin.getData()
+  data: {
+    touchStartTime: 0,
+    touchEndTime: 1694573918098,
+    touchDuration: 220,
+    absolute: '',
+    animation: [],
+    currentCardOption: 0,
+    currentItem: null,
+    currentItemIndex: 0,
+    currentCardPosition: {
+      x: 35,
+      y: 20
+    },
+    currentHideClass: '',
+    like: false,
+    dislike: false,
+    isDelete: false,
+    isFit: false,
+    list: [],
+    hidden: false,
+    titleBarHeight: 0,
+    statusBarHeight: 0,
+    rotate: 0,
+    position: '',
+  },
+
+  fetchData() {
+
+    var data = [];
+
+    for (var i = 1; i <= 10; i++) {
+      var entry = {
+        "id": i,
+        "type": "image",
+        "url": `https://picsum.photos/300/600?random=${i}`,
+        "title": `Card ${i}`,
+        "subtitle": `Card ${i} Subtitle`,
+        "description": `Card ${i} Description`,
+        "navigateUrl": `/pages/index/index?id=${i}`
+      };
+      data.push(entry);
+    }
+
+    this.setData({
+      list: data
+    })
+
+  },
+
+  reload() {
+    this.fetchData()
+    this.setData({
+      hidden: false,
+    })
+  },
+
+  onReady() {
+
+    const {
+      titleBarHeight,
+      statusBarHeight,
+    } = my.getSystemInfoSync();
+
+    this.setData({
+      titleBarHeight,
+      statusBarHeight,
+      currentCardPosition: {
+        x: 35,
+        y: 0
+      },
+    });
+
+    my.setCanPullDown({
+      canPullDown: false,
+    });
+
+    this.fetchData()
+  },
+
+  onTouchStart(e) {
+    this.setData({
+      touchStartTime: e.timeStamp
+    });
+  },
+
+  onSwiperChange(e) {
+    const {
+      windowWidth
+    } = my.getSystemInfoSync();
+
+    const swipe = Math.ceil(e.changedTouches[0].x)
+    const rotate = Math.ceil(((windowWidth / 2) - swipe) * -1)
+
+    if (rotate >= -200 && rotate <= 200) {
+      if (rotate < 0) {
+
+        this.setData({
+          position: "transform-origin: bottom left;",
+          absolute: "right: 0;"
+        });
+      }
+
+      if (rotate > 0) {
+        this.setData({
+          position: "transform-origin: bottom right;",
+          absolute: "left: 0;"
+        });
+      }
+      this.setData({
+        rotate: rotate,
+      });
+
+      if (rotate > 30) {
+        this.setData({
+          rotate: 30,
+          position: "transform-origin:right bottom;",
+          currentCardOption: 1,
+          currentCardPosition: {
+            x: 50,
+            y: 0
+          },
+          like: true,
+          dislike: false,
+        });
+      } else if (rotate < -30) {
+        this.setData({
+          rotate: -30,
+          position: "transform-origin:left bottom;",
+          currentCardPosition: {
+            x: 50,
+            y: 0
+          },
+          currentCardOption: -1,
+          dislike: true,
+          like: false,
+        });
+      } else {
+        this.setData({
+          currentCardOption: 0,
+          dislike: false,
+          like: false,
+          currentCardPosition: {
+            x: 50,
+            y: 0
+          },
+        });
+      }
+    }
+  },
+
+
+  onEndSwipe(e) {
+
+    setTimeout(() => {
+      this.setData({
+        touchEndTime: e.timeStamp,
+        touchDuration: e.timeStamp - this.data.touchStartTime,
+        rotate: 0,
+        currentCardOption: 0,
+        currentHideClass: '',
+        absolute: "left: 0;",
+        dislike: false,
+        like: false
+      })
+    }, 100);
+
+
+    const {
+      windowWidth
+    } = my.getSystemInfoSync();
+
+    const touchDuration = this.data.touchDuration
+
+    const swipe = Math.ceil(e.changedTouches[0].x)
+    const rotate = Math.ceil(((windowWidth / 2) - swipe) * -1)
+
+    if (touchDuration > 200) {
+      if (rotate <= -45) {
+        this.data.list.shift()
+
+        this.setData({
+          hidden: true,
+          list: this.data.list,
+          currentCardPosition: {
+            x: 34,
+            y: 0
+          },
+        })
+
+      } else if (rotate >= 45) {
+
+        this.data.list.shift()
+
+        this.setData({
+          hidden: true,
+          list: this.data.list,
+          currentCardPosition: {
+            x: 34,
+            y: 0
+          },
+          currentCardOption: 1,
+        })
+      }
+    }
+  },
+
+  handleSwipeLeft() {
+    this.data.list.shift()
+
+    this.setData({
+      currentCardOption: -1,
+      currentHideClass: 'card-hide-left',
+      isDelete: true,
+    })
+
+    setTimeout(() => {
+      this.setData({
+        currentCardOption: 0,
+        currentHideClass: '',
+        hidden: true,
+        isDelete: false,
+        list: this.data.list
+      })
+    }, 500);
+
+
+    if (!this.data.list.length) {
+      this.reload()
+    }
+  },
+
+  handleSwipeRight() {
+    this.data.list.shift()
+
+    this.setData({
+      currentCardOption: 1,
+      currentHideClass: 'card-hide-right',
+      isFit: true,
+    })
+
+    setTimeout(() => {
+      this.setData({
+        currentCardOption: 0,
+        currentHideClass: '',
+        hidden: true,
+        isFit: false,
+        list: this.data.list
+      });
+    }, 500);
+
+    if (!this.data.list.length) {
+      this.reload()
+    }
   }
+
 })
